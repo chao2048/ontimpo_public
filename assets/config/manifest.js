5e03dc419186f2125b593c333b06e9efac6452df
Chao Yi-Ju <78418487+chao2048@users.noreply.github.com>
2022-07-25 00:10:29 +0800

    Delete assets directory

--- assets/config/manifest.js
+++
@@ -1,8 +0,0 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= require moment
-//= require jquery
-//= require jquery_ujs
-//= require fullcalendar
