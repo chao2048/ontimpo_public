04cfcd49eb4dbd35e206b173e59b24201c445a7c
Chao Yi-Ju <78418487+chao2048@users.noreply.github.com>
2022-08-14 09:19:32 +0800

    Add files via upload

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,10 @@
+//= link_tree ../images
+//= link_tree ../audios
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= require moment
+//= require jquery
+//= require jquery_ujs
+//= require fullcalendar
+//= link application.css
